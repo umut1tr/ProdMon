0435839ad0bcc2398ddf738275cb9af19a5b7dbd
Umut Saw <umut1tr@hotmail.com>
2024-12-01 20:50:13 +0100

    init with base Architecture

---
+++ ProdMon.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdMon.Application
+{
+    public static class DependencyInjection
+    {
+        // eventuell später MediatR pattern Service hier einbauen.
+
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
+
+    }
+}
---
+++ ProdMon.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdMon.Infrastructure
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            // eventuelle services später? Einträge des Fremdsystems + Artikelcodes für "Artikelnummer (Key) , + Produktname)??
+
+            // services.AddScoped<IQualityControlEntryRepository, QualityControlEntryRepository>();
+            // services.AddScoped<IArtikelcodeRepository, ArtikelCodeRepository>();
+
+            return services;
+        }
+
+
+    }
+}
