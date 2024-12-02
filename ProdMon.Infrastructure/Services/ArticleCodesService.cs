ced32b79e559133da95d20994097ee4d619dfd6b
Umut Saw <umut1tr@hotmail.com>
2024-12-02 19:26:25 +0100

    added ArticleCodesService blueprint added MonitorEntriesService blueprint - for later usage in the Razor. Components

---
+++ ProdMon.Infrastructure/Services/ArticleCodesService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdMon.Infrastructure.Services
+{
+    internal class ArticleCodesService
+    {
+        // da der Ansatz über die Application layer nicht funktionier bei mir lokal ausweichen auf diesen Service um in der Razor komponente die ArticleCodes zu verwalten.
+    }
+}
