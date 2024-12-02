453810ea4e82c446443cf6519577b9f79edbde00
Umut Saw <umut1tr@hotmail.com>
2024-12-02 19:32:11 +0100

    added seeding for default ArticleCode "Querlenker"

--- ProdMon.Infrastructure/Data/ProdMonDbContext.cs
+++ ProdMon.Infrastructure/Data/ProdMonDbContext.cs
@@ -10,6 +10,21 @@ namespace ProdMon.Infrastructure.Data
         public DbSet<ArticleCode> ArticleCodes { get; set; }
         public DbSet<MonitorEntry> MonitorEntries { get; set; }
 
-        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seed some basic Data for ArticleCodes for later testing
+
+            modelBuilder.Entity<ArticleCode>().HasData(
+                new ArticleCode
+                {
+                    ArticleNumber = 5322234,
+                    ArticleDescription = "Querlenker"
+                }
+
+            );
+        }
+
     }
 }
