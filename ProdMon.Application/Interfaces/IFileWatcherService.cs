3485a5656cb305d2c0d09dd0d37addc20b1116c2
Umut Saw <umut1tr@hotmail.com>
2024-12-03 23:07:15 +0100

    comment fix

--- ProdMon.Application/Interfaces/IFileWatcherService.cs
+++ ProdMon.Application/Interfaces/IFileWatcherService.cs
@@ -8,7 +8,7 @@ namespace ProdMon.Application.Interfaces
         Task StartAsync(CancellationToken cancellationToken);
         Task StopAsync(CancellationToken cancellationToken);
 
-        //// Event, das ausgelöst wird, wenn sich die Datei geändert hat um dem FrontEnd zu sagen, dass es neu laden soll        
+        // Signal to FrontEnd   
         event Action? EntryHasBeenApplied;
     }
 }
